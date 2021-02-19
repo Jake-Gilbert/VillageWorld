63093b841f471a9e97f3747fad1d6dcaf65a0da6
Jake Gilbert <gilberj1@aston.ac.uk>
2021-02-19 20:46:04 +0000

    Agents have vision
    Agents now see fruit bushes and move towards them, they don't pick up fruit yet

--- Assets/Scripts/WorldScripts/FruitBush.cs
+++ Assets/Scripts/WorldScripts/FruitBush.cs
@@ -16,17 +16,13 @@ public class FruitBush : MonoBehaviour
         {
             GameObject rotationPoint = new GameObject();
             rotationPoint.transform.position = bush.transform.position;
-            GameObject fruit = (GameObject)Instantiate(Resources.Load("Fruit"), bush.transform.position + new Vector3(0,  collider.radius * 2.8F, 0), Quaternion.identity);
+            GameObject fruit = (GameObject)Instantiate(Resources.Load("Fruit"), bush.transform.position + new Vector3(0,  collider.radius * 2.9F, 0), Quaternion.identity);
             fruit.transform.parent = rotationPoint.transform;
-            rotationPoint.transform.eulerAngles = new Vector3(Random.Range(45,180), rotationPoint.transform.eulerAngles.y, Random.Range(1, 180));
+            rotationPoint.transform.eulerAngles = new Vector3(Random.Range(-100,100), Random.Range(-50, 50), Random.Range(-100, 100));
             fruit.transform.parent = bush.transform;
             Destroy(rotationPoint.gameObject);
         }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
